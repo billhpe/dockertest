4a483699b4ec6db5ee81b41fd695198bc72cc588
Bill Johnson <billj@hpe.com>
2022-03-18 12:44:52 -0500

    Updated message

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Flask app alive and well</h2>'
+    return '<h1>Flask app updated and pushed</h2>'
 
 
 if __name__ == "__main__":
