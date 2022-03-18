9ca137ed89f9d1ced73470c82b6aec0f7b64ba03
Bill Johnson <billj@hpe.com>
2022-03-18 14:03:46 -0500

    First attempt at adding unit tests into the workflow

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+def get_message():
+    return "Hello unit test"
+
 @app.route('/')
 def home():
-    return '<h1>Fixed the issue</h2>'
+    return '<h1>' + get_message() + '</h2>'
 
 
 if __name__ == "__main__":
