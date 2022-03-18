77112b9629a1bcf98b5ec223b101c83d6b5d3c84
Bill Johnson <billj@hpe.com>
2022-03-18 14:08:43 -0500

    Fixed intentionally broken unit test

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from app import get_message
 class TestApp(unittest.TestCase):
     def test_message(self):
         msg = get_message()
-        self.assertEqual(msg, "Hello unit test!")
+        self.assertEqual(msg, "Hello unit test")
 
 if __name__ == '__main__':
     unittest.main()
